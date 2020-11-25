6728938c5fba8c35d32c0c5864fcf262f55cb7c4
Rodrigo Galdino <63233782+rodrigogaldino553@users.noreply.github.com>
2020-11-25 20:21:56 -0200

    fix bugs

--- main.js
+++ main.js
@@ -36,7 +36,6 @@ async function request_poke(name) {
 }
 
 function create_card(pokemon) {
-  console.log(pokemon)
   card = `<div class="card poke-card ${pokemon.types[0].type.name.toString()}" style="width: 18rem;">
   <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="Sprite of ${pokemon.name}" class="card-img-top">
   <div class="card-body">
@@ -46,21 +45,7 @@ function create_card(pokemon) {
     <p class="card-text">
     ${pokemon.weight / 10}kg | ${pokemon.height / 10}m</p>
   </div>
-</div>`
-  /*</div>card = `
-      <h3>Showing results for "${poke_name}"</h3>
-      <div class="pokemon-picture">
-        <img width="200px" height="200px" class="pokemon-sprite" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="Sprite of ${pokemon.name}">
-      </div>
-      <div class="pokemon-info">
-        <h1 class="name">${pokemon.name}</h1>
-        
-        <h2 class="number">N° ${pokemon.id}</h2>
-        <h3 class="type"><a id="title">Type:</a> ${pokemon.types.map(item => ' ' + item.type.name).toString()}</h3>
-        <h3 class="skill"><a id="title">Skills:</a> ${pokemon.moves.map(item => ' ' + item.move.name).toString()}</h3>
-        <h3 class="weight"><a id="title">Weight:</a> ${pokemon.weight / 10}kg</h3>
-        <h3 class="height"><a id="title">Height:</a> ${pokemon.height / 10}m</h3>
-      </div>`;*/
+  </div>`
 
   return card
 }
@@ -78,7 +63,7 @@ async function start_app() {
   } catch (error) {
     showAlert(`Error! not was possible get "${poke_name}"`, 'alert-danger')
     search_container.innerHTML = ''
-    container.innerHTML = `<h3>Error! not was possible get "${poke_name}"</h3>`
+    container.innerHTML = `<h3>Error! can't get "${poke_name}"</h3>`
   }
 }
 
@@ -112,7 +97,6 @@ search_button.addEventListener('click', event => {
 })
 
 
-//showAlert('Hello world', 'alert-danger')
 function showAlert(text, alertClass) {
   const alertField = document.querySelector('#alert')
   alertField.classList.remove('hide')
